8595c5a3c4fc7fd70c3e6ab9e16e427a0b42c55c
Michael Randall <1154334+MichaelRandall@users.noreply.github.com>
2025-11-14 15:07:02 -0800

    Implement Lambda function to get friends from DynamoDB

---
+++ infrastructure/lambda/get-friends/index.js
@@ -0,0 +1,46 @@
+const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
+const { DynamoDBDocumentClient, ScanCommand } = require('@aws-sdk/lib-dynamodb');
+
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+
+const TABLE_NAME = process.env.TABLE_NAME;
+
+exports.handler = async (event) => {
+  console.log('Get Friends - Event:', JSON.stringify(event, null, 2));
+
+  try {
+    const command = new ScanCommand({
+      TableName: TABLE_NAME,
+    });
+
+    const response = await docClient.send(command);
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET,OPTIONS'
+      },
+      body: JSON.stringify({
+        friends: response.Items || [],
+        count: response.Count || 0
+      })
+    };
+  } catch (error) {
+    console.error('Error:', error);
+    return {
+      statusCode: 500,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        error: 'Failed to retrieve friends',
+        message: error.message
+      })
+    };
+  }
+};
